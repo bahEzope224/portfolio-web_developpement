9628412f99ed3f25515766acf6e0a356fa34540b
Bah Ezope <ezope92@gmail>
2025-09-26 01:47:46 +0200

    maj portfolio data analyst

---
+++ build/precache-manifest.609eeac9a00b96e37a1eb1ef325d64e2.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5c6829e7b9033067e86c5ba8e79c7ca2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1b49010232be64343692",
+    "url": "/static/css/2.d37f0467.chunk.css"
+  },
+  {
+    "revision": "b089d1c8f827ca864caa",
+    "url": "/static/css/main.6be40ec6.chunk.css"
+  },
+  {
+    "revision": "1b49010232be64343692",
+    "url": "/static/js/2.e02dd3ea.chunk.js"
+  },
+  {
+    "revision": "3282da49cbe763b0eb8ce4d733763b3a",
+    "url": "/static/js/2.e02dd3ea.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "01f684bb4c5f459c7d6e",
+    "url": "/static/js/3.30f3bf4f.chunk.js"
+  },
+  {
+    "revision": "af1eb437f46c43138edb",
+    "url": "/static/js/4.d5de5761.chunk.js"
+  },
+  {
+    "revision": "b089d1c8f827ca864caa",
+    "url": "/static/js/main.80244367.chunk.js"
+  },
+  {
+    "revision": "f510f2a0a034070f377b",
+    "url": "/static/js/runtime-main.cae91cb8.js"
+  },
+  {
+    "revision": "a2f04d20d0e26c149bc78ee48cb4f5b7",
+    "url": "/static/media/CV_2025-09-03_Ibrahima_Bah.a2f04d20.pdf"
+  },
+  {
+    "revision": "8880a7d21cedd1d1167604c429161419",
+    "url": "/static/media/about.eedcc122.png"
+  },
+  {
+    "revision": "0c22540a5401c96a3e99f9468df9bc07",
+    "url": "/static/media/avatar.6a797c75.png"
+  },
+  {
+    "revision": "8e45b5e13a451923b43314ccd5142d27",
+    "url": "/static/media/disneyplus.82757861.png"
+  },
+  {
+    "revision": "787fa41f3faac7d96c798a901c10dfaf",
+    "url": "/static/media/home-bg.a5710d16.jpg"
+  },
+  {
+    "revision": "9516a599833e9a06972ef8ba1b617236",
+    "url": "/static/media/home-main.9516a599.svg"
+  },
+  {
+    "revision": "9bd375ac65d8c2cb3bfedba561187475",
+    "url": "/static/media/img-savenelec.509af5fa.png"
+  },
+  {
+    "revision": "d2b581250321a84df35f290cd6f187a6",
+    "url": "/static/media/jo2024.4c910900.png"
+  },
+  {
+    "revision": "a5e3ae01495e04bda7f3a3a456306768",
+    "url": "/static/media/perfobourse.77131e0f.png"
+  },
+  {
+    "revision": "6030f73e92394aa11c1deca907e91732",
+    "url": "/static/media/pre.6030f73e.svg"
+  },
+  {
+    "revision": "7579be5fa34b6e4756de63e92cecea04",
+    "url": "/static/media/projet-biblo.943d96dc.png"
+  },
+  {
+    "revision": "c80795f36ca67488c0833e27f2c21bcd",
+    "url": "/static/media/qalamsoftware.6c96520c.png"
+  },
+  {
+    "revision": "921f89592b5dddadffb813b11e08ef63",
+    "url": "/static/media/veille-technologique.a9f59aae.png"
+  }
+]);
--- src/components/About/AboutCard.js
+++ src/components/About/AboutCard.js
@@ -7,18 +7,36 @@ function AboutCard() {
     <Card className="quote-card-view">
       <Card.Body>
         <blockquote className="blockquote mb-0">
-        <p style={{ textAlign: "justify" }}>
-    Bonjour, Je suis <span className="yellow">Ibrahima BAH</span>, <br />
+        <Card.Body>
+  <blockquote className="blockquote mb-0">
+
+    <h1>Parcours : <strong className="purple">BTS SIO</strong> vers <strong className="purple">Licence Professionnelle Data Mining & Statistique Décisionnelle</strong></h1>
     <br />
-    J'ai obtenu mon <strong className="purple">BTS SIO</strong> (Services informatiques aux Organisations) option <strong className="purple">SLAM</strong> (Solutions Logicielles et Applications Métiers).
-    <br />
-    Je suis actuellement en recherche d'alternance pour un <strong className="purple">bac+3</strong> en <strong className="purple">programmation web et application</strong> pour la rentrée 2025.
-    <br />
-    J'ai également obtenu un <strong className="purple">baccalauréat général</strong> avec les spécialités <strong className="purple">Physique-Chimie</strong> et <strong className="purple">Numérique et Sciences de l'informatique</strong>.
-    <br />
-    <br />
-    Mise à part la programmation, voici d'autres activités que j'adore faire :
-</p>
+
+    <p style={{ textAlign: "justify" }}>
+      Après l’obtention de mon <strong className="purple">BTS Services Informatiques aux Organisations (option SLAM)</strong>, 
+      j’ai choisi de me réorienter en <strong className="purple">Licence Professionnelle Data Mining & Statistique Décisionnelle</strong> 
+      afin d’approfondir mes compétences en <strong className="purple">analyse de données</strong> et de me spécialiser 
+      dans le domaine de la <strong className="purple">Big Data</strong>.
+      <br /><br />
+      Cette formation me permet de développer des compétences avancées en <strong className="purple">statistique appliquée</strong>, 
+      <strong className="purple">programmation (Python, R, SQL)</strong>, 
+      <strong className="purple">data mining</strong> et <strong className="purple">visualisation de données</strong>, 
+      dans le but de résoudre des problématiques réelles d’aide à la décision.
+      <br /><br />
+      Je suis actuellement à la recherche :
+      <ul>
+        <li>d’une <strong className="purple">alternance</strong> en <strong className="purple">data analyse</strong> (rythme : 3 jours en entreprise / 2 jours à l’université),</li>
+        <li>ou d’un <strong className="purple">stage de 2 mois minimum à partir de juin</strong>,</li>
+        <li>ou d’un <strong className="purple">stage alterné</strong> dès maintenant.</li>
+      </ul>
+      Mon objectif est de mettre à profit mes compétences en <strong className="purple">Big Data </strong> 
+      et en <strong className="purple">analyse statistique</strong> au sein d’une entreprise innovante.
+    </p>
+
+  </blockquote>
+</Card.Body>
+
 
           <ul>
           <li className="about-activity">
